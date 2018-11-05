833b1162f592405c6233072ddc444bf192120d12
MP7373 <martinpayne73@gmail.com>
2018-11-04 22:13:50 -0500

    fixed fcc tests script link from href to src... oops lol

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import SheetOfPaper from './SheetOfPaper.js';
 class App extends Component {
   render() {
     return (
       <div className="App">
-        
+        I'm App.js!
+        <SheetOfPaper />
       </div>
     );
   }
--- src/SheetOfPaper.js
+++ src/SheetOfPaper.js
@@ -4,8 +4,10 @@ class SheetOfPaper extends Component {
   render() {
     return (
       <div className="sheet-of-paper">
-        
+        <h1>I'm a paper sheet!</h1>
       </div>
     );
   }
 }
+
+export default SheetOfPaper;
